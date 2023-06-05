417b84151bb622ba9587ca3d6303111498c7f559
Utsav sharma <73983697+UtsavsmSharma@users.noreply.github.com>
2023-06-05 09:05:13 +0530

    new branch

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import *
 import os
 import socket
+from ddtrace import tracer
+
+tracer.configure(
+    hostname='datadog-agent',
+    port=8126,
+)
 
 app = Flask(__name__)
 
@@ -15,9 +21,9 @@ if __name__ == '__main__':
     app.run()
 
 
-PORT = int(os.environ.get("PORT", 9090))
+# PORT = int(os.environ.get("PORT", 9090))
 if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=PORT)
+    app.run(threaded=True,host='0.0.0.0',port=9090)
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=9090)
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', port=9090)
